7505e0f5a7ed61ade1e1d3628761638f6b35edac
feysel <feys77@gmail.com>
2024-11-05 00:26:50 -0800

    configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/NETFLIX-F-CLONE/",
-  plugins: [react()],
-});
+// vite.config.js
+export default {
+  base: '/NETFLEX-F-CLONE/', // Make sure this is your GitHub Pages URL path
+  build: {
+    outDir: 'dist' // This is the default, but it's good to confirm
+  }
+}
