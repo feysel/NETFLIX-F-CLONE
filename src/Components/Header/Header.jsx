509a1f0533793f2726e135b4fe68f017bd97b42b
feysel <feys77@gmail.com>
2024-11-05 01:49:11 -0800

    Fix image path for netflix blacklogo.jpg

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../Header/header.css";
-import NetflixLogo from "../../assets/Images/netflix blacklogo.jpg";
+import NetflixLogo from "../../assets/Images/Netflix_blacklogo";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationAddIcon from "@mui/icons-material/NotificationAdd";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
